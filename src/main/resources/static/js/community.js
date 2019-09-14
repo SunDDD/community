0051f0d8f29e6684f9fdd26b37efe4a4ec13bbfc
SunDDD <houwenjun0992@126.com>
2019-09-14 19:31:08 +0800

    无刷新登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,4 +1,3 @@
-
 function post() {
     var questionId = $("#question_id").val();
     var commentContent = $("#comment_content").val();
@@ -7,15 +6,21 @@ function post() {
         url: "/comment",
         contentType: "application/json",
         data: JSON.stringify({
-            "parentId" : questionId,
-            "content" : commentContent,
-            "type" : 1
+            "parentId": questionId,
+            "content": commentContent,
+            "type": 1
         }),
         success: function (response) {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    window.open("https://github.com/login/oauth/authorize?client_id=e59f3b8eca5fa102d26f&redirect_uri=http://localhost:8287/callback&scope=user&state=1")
+                    window.localStorage.setItem("closable", true);
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
